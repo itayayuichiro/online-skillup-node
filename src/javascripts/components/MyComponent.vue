57204cb6d13bf4202e8ba25481d9450a6a9e5e85
Yuichiro Itaya <mr.itaya.magic@gmail.com>
2019-01-29 15:57:31 +0900

    init

---
+++ src/javascripts/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul id="example-1">
+            <li v-for="message in $props.messages">
+                {{ message.text }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  props: {
+    messages: VueTypes.isRequired
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
